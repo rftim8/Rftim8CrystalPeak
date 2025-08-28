0c2c4efdfd27c0a8462f2c03f536d8ac0c324ed4
rftim8 <rftim8@outlook.com>
2025-08-28 12:08:14 +0300

    Add project files.

---
+++ Rftim8CrystalPeak/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Rftim8CrystalPeak
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ArgumentNullException.ThrowIfNull(args);
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
