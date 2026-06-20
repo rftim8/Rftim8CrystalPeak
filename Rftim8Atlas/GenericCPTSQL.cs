085c6a9f8b792486129ea323c60b73055eabab59
Remus Timbuli <rftim8@outlook.com>
2026-06-20 16:10:31 +0300

    - environment change

--- Rftim8Atlas/GenericCPTSQL.cs
+++ Rftim8Atlas/GenericCPTSQL.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore.Storage;
 using Rftim8Atlas.Models.CP;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rftim8Atlas
 {
--- Rftim8Atlas/GenericURLs.cs
+++ Rftim8Atlas/GenericURLs.cs
@@ -8,7 +8,7 @@ namespace Rftim8Atlas
 {
     public class GenericURLs
     {
-        public static readonly string data_base_folder = "D:\\Rftim8Atlas\\";
+        public static readonly string data_base_folder = "D:\\RFT1\\Projects\\Rftim8Atlas\\";
         public static readonly string mssqlDb = "Data Source=RFT1; Initial Catalog=CrystalPeak; Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
     }
 }
